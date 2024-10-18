d3db429f27e47a1ccc9aece09590f7ef3c1ecbe3
wongakim-99 <202021000@sangmyung.kr>
2024-10-18 15:57:41 +0900

    #5 - Callback 에 대해서 공부
    콜백(Callback) 은 다른 함수에 인자로 전달되는 함수를 의미한다. 콜백 함수는 전달된 함수가 실행되는 시점에 호출됨. 즉, 콜백은 특정 작업이 완료된 후에 호출되도록 설계된 함수

---
+++ src/callback.ts
@@ -0,0 +1,13 @@
+// 콜백의 기본 개념 예시
+function greeting(name: string) {
+    console.log(`Hello, ${name}`);
+}
+
+/**prompt 함수가 string | null 타입을 반환할 수 있기 때문에 발생합니다. 사용자가 취소를 누르면 null을 반환하기 때문이죠. 
+ * 하지만 callback 함수는 string 타입만 허용하고 있어서 타입 오류가 발생합니다. */
+function processUserInput(callback: (name: string) => void) {
+    const name = 'gawon';
+    callback(name);
+}
+
+processUserInput(greeting);
